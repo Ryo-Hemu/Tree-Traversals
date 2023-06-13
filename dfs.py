ffb1b5814abed385f9b678d445d3532c72ed143a
Hemanth <hemanth25damarla@gmail.com>
2023-06-13 20:48:43 +0530

    dfs or pre-order traversal

---
+++ dfs.py
@@ -0,0 +1,23 @@
+# Depth-First Search or Pre-Order Traversal
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root):
+        if not root:
+            return []
+        result, stack = [], [root]
+        while stack:
+            current = stack.pop()
+            result.append(current.val)
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+        return result
