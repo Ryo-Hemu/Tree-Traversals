53057d3873dac2b1852dde5a7faa9d9d201c2acb
Hemanth <hemanth25damarla@gmail.com>
2023-06-14 12:14:14 +0530

    level order traversal / bfs

---
+++ bfs.py
@@ -0,0 +1,28 @@
+# Breadth-First Search or Level Order Traversal
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def bfs(self, root):
+        from collections import deque
+
+        if not root:
+            return []
+
+        result, queue = [], deque()
+        queue.append(root)
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
